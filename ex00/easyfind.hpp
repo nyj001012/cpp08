31c8e2460a9f37af471cf1ec14d4ec46ad3a8053
yena <50291995+nyj001012@users.noreply.github.com>
2023-10-14 15:22:57 +0900

    feat(ex00): implement ex00

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 15:00:21 by yena              #+#    #+#             */
+/*   Updated: 2023/10/14 15:07:56 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//
+// Created by 나예진 on 2023/10/14.
+//
+
+#ifndef EX00__EASYFIND_HPP_
+#define EX00__EASYFIND_HPP_
+
+#include <iostream>
+#include "font.hpp"
+
+template<typename T>
+int easyfind(T &container, int n) {
+  typename T::iterator it;
+
+  it = std::find(container.begin(), container.end(), n);
+  if (it == container.end())
+    throw std::exception();
+  return *it;
+}
+
+#endif //EX00__EASYFIND_HPP_
---
+++ ex00/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 14:55:35 by yena              #+#    #+#             */
+/*   Updated: 2023/10/14 15:22:34 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+
+int main() {
+  std::cout << F_GREEN
+            << "-------------------- EASY FIND --------------------" << std::endl
+            << FB_DEFAULT;
+  std::srand(std::time(NULL));
+  std::vector<int> int_vec(5);
+  std::cout << "int_vec: { ";
+  for (std::vector<int>::iterator it = int_vec.begin(); it != int_vec.end(); it++) {
+    *it = std::rand() % 10;
+    std::cout << *it << " ";
+  }
+  std::cout << "}" << std::endl;
+  std::cout << "easyfind(int_vec, 5): " << std::endl;
+  try {
+    int found_value = easyfind(int_vec, 5);
+    std::cout << F_CYAN << "FOUND: " << found_value << FB_DEFAULT << std::endl;
+  } catch (std::exception &e) {
+    std::cout << F_RED << "NOT FOUND" << FB_DEFAULT << std::endl;
+  }
+}
