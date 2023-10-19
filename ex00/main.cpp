09b9dc12524f157d3b6c13186c57679562876463
yena <nyj001012@gmail.com>
2023-10-19 16:19:25 +0900

    fix: :rotating_light: include vector header

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,23 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: yena <yena@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 14:55:35 by yena              #+#    #+#             */
-/*   Updated: 2023/10/14 15:22:34 by yena             ###   ########.fr       */
+/*   Updated: 2023/10/19 16:17:56 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <vector>
 
 #include "easyfind.hpp"
 
 int main() {
-  std::cout << F_GREEN
-            << "-------------------- EASY FIND --------------------" << std::endl
+  std::cout << F_GREEN << "-------------------- EASY FIND --------------------"
+            << std::endl
             << FB_DEFAULT;
   std::srand(std::time(NULL));
   std::vector<int> int_vec(5);
   std::cout << "int_vec: { ";
-  for (std::vector<int>::iterator it = int_vec.begin(); it != int_vec.end(); it++) {
+  for (std::vector<int>::iterator it = int_vec.begin(); it != int_vec.end();
+       it++) {
     *it = std::rand() % 10;
     std::cout << *it << " ";
   }
